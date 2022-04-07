59fdc28ca6e22f11920e3e77d2c2f5785f2ab5b2
spexsog <spexsog@gmail.com>
2022-04-07 11:38:13 -0700

    Transition work

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,23 @@ const modalAboutMe = document.querySelector("#modal-about-me");
 
 function initEventListeners(){
     document.querySelector('#link-about-me').addEventListener('click',(e)=>{
+        modalAboutMe.classList.toggle('transition');
+        modalAboutMe.clientWidth;
         modalAboutMe.classList.toggle('hide');
         e.preventDefault();
     });
+
     modalAboutMe.addEventListener('click',(e)=>{
         modalAboutMe.classList.toggle('hide');
     });
+
+    modalAboutMe.addEventListener('transitionend', (e)=>{
+        console.log('done');
+        modalAboutMe.classList.remove('transition');
+    });
 }
+
+
 
 initEventListeners();
 
