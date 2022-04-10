6ef63929666cd324d7c2d6f4e2c805bacefe3edb
spexsog <spexsog@gmail.com>
2022-04-10 12:00:06 -0700

    Commenting

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,24 @@ const container = document.querySelector('#container');
 const expUL = document.querySelector('#experience');
 const modalList = document.querySelectorAll('.modal');
 
+// Initialize Event Listeners
 function initEventListeners(){
+
+    // Click Event for Entire Body
     body.addEventListener('click',(e)=>{
+
+        // Checking to see if any modal is visible to toggle visibility
         modalList.forEach((modal)=>{
             if(!modal.classList.contains('hide')){
                 toggleModal(modal);
             }
         });
-        
+
+        // list of link ids
         let idList=['link-about-me','link-portfolio','link-services','link-contact'];
+
+        // Checking to see if click event occurred on any modal links
+        // to toggle modal visibility
         if(idList.includes(e.target.id)){
             targetModal = `#modal-${e.target.id.slice(5)}`;
             toggleModal(document.querySelector(targetModal));
@@ -20,17 +29,18 @@ function initEventListeners(){
         }
         
     });
-
-    function highlightExp() {
-        let lis = Array.from(expUL.children);
-        lis.forEach((li)=>{
-            let strArray = li.innerHTML.split('-');
-            let highlightTxt = '<span class="highlight"> &mdash; </span>';
-            li.innerHTML = strArray.join(highlightTxt);
-        })
-    }
 }
 
+// Function to replace all dashes with em dash currently in just 
+// experience
+function highlightExp() {
+    let lis = Array.from(expUL.children);
+    lis.forEach((li)=>{
+        li.innerHTML = li.innerHTML.replaceAll('-','<span class="highlight"> &mdash; </span>');
+    })
+}
+
+// Function to toggle the passed modal
 function toggleModal(modal){
     modal.classList.toggle('transition');
     modal.clientWidth;
@@ -42,5 +52,6 @@ function toggleModal(modal){
     });
 }
 
+// Function call to initialize event listeners
 initEventListeners();
 
